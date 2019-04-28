cb662dd71c29d3151fcbcc719493feb992df42b5
johnsmith <45Duplex>
2019-04-28 23:31:18 +0300

    Response from lois

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ io.on('connection', (socket) => {
         //broadcast the new message
         io.sockets.emit('new_message', {message : data.message, username : socket.username});
         // Pass an utterance to the sample LUIS app
-         getLuisIntent('Hello!');
+        getLuisIntent(data.message);
+
     })
 
     //listen on typing
@@ -49,8 +50,9 @@ io.on('connection', (socket) => {
     })
 })
 
-var request = require('request');
-var querystring = require('querystring');
+let request = require('request');
+let querystring = require('querystring');
+let customEnv = require('custom-env').env();
 
 // Analyze text
 //
@@ -59,28 +61,28 @@ var querystring = require('querystring');
 function getLuisIntent(utterance) {
 
     // endpoint URL
-    var endpoint =
+    let endpoint =
         "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/";
 
     // Set the LUIS_APP_ID environment variable
     // to df67dcdb-c37d-46af-88e1-8b97951ca1c2, which is the ID
     // of a public sample application.
-    var luisAppId = process.env.LUIS_APP_ID;
+    let luisAppId = process.env.LUIS_APP_ID;
 
     // Read LUIS key from environment file ".env"
     // You can use the authoring key instead of the endpoint key.
     // The authoring key allows 1000 endpoint queries a month.
-    var endpointKey = process.env.LUIS_ENDPOINT_KEY;
+    let endpointKey = process.env.LUIS_ENDPOINT_KEY;
            
     // Create query string
-    var queryParams = {
+    let queryParams = {
         "verbose":  true,
         "q": utterance,
         "subscription-key": endpointKey
     }
 
     // append query string to endpoint URL
-    var luisRequest =
+    let luisRequest =
         endpoint + luisAppId +
         '?' + querystring.stringify(queryParams);
 
@@ -92,14 +94,18 @@ function getLuisIntent(utterance) {
             if (err)
                 console.log(err);
             else {
-                var data = JSON.parse(body);
-                               console.log(data);
-                console.log(`Query: ${data.query}`);
+                let data = JSON.parse(body);
+
+                console.log(data);
                 console.log(`Top Intent: ${data.topScoringIntent.intent}`);
-                console.log('Intents:');
-                console.log(JSON.stringify(data.intents));
+                console.log(JSON.stringify(data.intents[0].intent));
+
+                let louisSay = 'You reached the '+JSON.stringify(data.intents[0].intent+'. You said ' + utterance);
+                io.sockets.emit('new_message', {message : louisSay, username : 'Bot'});
+
             }
         });
+
 }
 
 
